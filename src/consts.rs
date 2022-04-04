4effb7a91fab61b0e513dab3505c66e79ae6a979
Nils Pukropp <nils@nirusu.codes>
2022-04-04 03:31:03 +0200

    implemented conversion from qoi + error management

---
+++ src/consts.rs
@@ -0,0 +1,7 @@
+pub const INVALID_SOURCE_FILE_EXT: &'static str = "invalid source file extension";
+pub const INVALID_DEST_FILE_EXT: &'static str = "invalid destination file extension";
+pub const INVALID_FILE_EXT: &'static str = "invalid destination or source file extension";
+pub const IMAGE_FORMATS: [&'static str; 19] = [
+    "png", "jgp", "jpeg", "webp", "ff", "exr", "hdr", "ico", "bmp", "dds", "tga", "tiff", "tif",
+    "pbm", "pam", "ppm", "pgm", "gif", "qoi",
+];
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::path::PathBuf;
 
 use clap::Parser;
-use qoi::encode_to_vec;
+use image::ColorType;
+use qoi::{decode_to_vec, encode_to_vec, Channels};
+
+use consts::*;
+
+mod consts;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -15,10 +20,67 @@ struct Args {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
-    let img = image::io::Reader::open(args.source)?.decode()?;
-    let width = img.width();
-    let height = img.height();
-    let encoded_qoi = encode_to_vec(&img.into_bytes(), width, height)?;
-    std::fs::write(args.destination, encoded_qoi)?;
-    Ok(())
+    let source_ext = match args.source.extension() {
+        Some(ext) => ext.to_str(),
+        None => {
+            println!("Error: {}", INVALID_SOURCE_FILE_EXT);
+            return Ok(());
+        }
+    };
+
+    let dest_ext = match args.destination.extension() {
+        Some(ext) => ext.to_str(),
+        None => {
+            println!("Error: {}", INVALID_DEST_FILE_EXT);
+            return Ok(());
+        }
+    };
+
+    match (source_ext, dest_ext) {
+        (Some("qoi"), Some("qoi")) => {
+            std::fs::copy(&args.source, &args.destination)?;
+            return Ok(());
+        }
+        (Some(source_ext), Some(dest_ext)) => {
+            let src_is_image = is_image_ext(source_ext);
+            if dest_ext == "qoi" && src_is_image {
+                let img = image::io::Reader::open(&args.source)?.decode()?;
+                let width = img.width();
+                let height = img.height();
+                let encoded_bytes = encode_to_vec(&img.into_bytes(), width, height)?;
+                std::fs::write(args.destination, encoded_bytes)?;
+            } else if source_ext == "qoi" && is_image_ext(dest_ext) {
+                let read = std::fs::read(&args.source)?;
+                let decoded_qoi = decode_to_vec(&read)?;
+                let color = match decoded_qoi.0.channels {
+                    Channels::Rgb => ColorType::Rgb8,
+                    Channels::Rgba => ColorType::Rgba8,
+                };
+                image::save_buffer(
+                    args.destination,
+                    &decoded_qoi.1,
+                    decoded_qoi.0.width,
+                    decoded_qoi.0.height,
+                    color,
+                )?;
+            } else {
+                println!(
+                    "Error: {} = {} -> {}",
+                    INVALID_FILE_EXT, source_ext, dest_ext
+                );
+            }
+            return Ok(());
+        }
+        (source_ext, dest_ext) => {
+            println!(
+                "Error: {} = {:?} -> {:?}",
+                INVALID_FILE_EXT, source_ext, dest_ext
+            );
+            return Ok(());
+        }
+    }
+}
+
+fn is_image_ext(ext: &str) -> bool {
+    IMAGE_FORMATS.iter().any(|s| *s == ext)
 }
